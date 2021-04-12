3d12bf463fe5f19b78db33d564cd4b57c9fb7df5
Ayaabudagar <ayakhlil23@yahoo.com>
2021-04-12 18:09:31 +0300

    creat a website about me

---
+++ js/app.js
@@ -0,0 +1,60 @@
+'use strict';
+let userName = prompt("please enter your name", "your name").toLowerCase();
+// console.log(userName);
+alert("thank you for visiting my page " + userName);
+
+let myName = prompt ("my name is aya?");
+if (myName.toLowerCase()==="yes"||myName.toLowerCase()==="y") {
+    // console.log("thats corecct my name is aya!");
+    alert("thats corecct my name is aya!");
+}  else if (myName.toLowerCase()==="no"||myName.toLowerCase()==="n") {
+//    console.log("thats false my name is aya!");
+    alert("thats false my name is aya!");
+
+  }
+
+
+  let myAge = prompt ("my age is 23?");
+if (myAge.toLowerCase()==="yes"||myAge.toLowerCase()==="y") {
+    // console.log("thats corecct my age is 23!");
+    alert("thats corecct my age is 23!");
+}  else if (myAge.toLowerCase()==="no"||myAge.toLowerCase()==="n") {
+    // console.log ("thats false my age is 23!");
+    alert("thats false my age is 23!");
+
+  }
+
+  let myEducation = prompt (" did i study architecture in university?");
+if (myEducation.toLowerCase()==="yes"||myEducation.toLowerCase()==="y") {
+    // console.log("thats corecct i'm studied architecture in university!");
+    alert("thats corecct i'm studied architecture in university!");
+}  else if (myEducation.toLowerCase()==="no"||myEducation.toLowerCase()==="n") {
+//    console.log("thats false i'm  studied architecture in university !");
+    alert("thats false i'm  studied architecture in university !");
+
+  }
+  
+  let myGraduation = prompt (" did i graduate from  university?");
+if (myGraduation.toLowerCase()==="yes"||myGraduation.toLowerCase()==="y") {
+    // console.log("thats corecct i'm graduated from university!");
+    alert("thats corecct i'm graduated from university!");
+}  else if (myGraduation.toLowerCase()==="no"||myGraduation.toLowerCase()==="n") {
+    // console.log("thats false i'm  graduated from university!");
+    alert("thats false i'm  graduated from university!");
+
+  }
+   
+  let myExperience = prompt (" did i have a work experience ?");
+if (myExperience.toLowerCase()==="yes"||myExperience.toLowerCase()==="y") {
+  // console.log("thats false i'm fresh graduated so i'm don't have work experience!");
+    alert("thats false i'm fresh graduated so i'm don't have work experience!");
+}  else if (myExperience.toLowerCase()==="no"||myExperience.toLowerCase()==="n") {
+//    console.log("thats corecct i'm fresh graduated so i'm don't have work experience!");
+    alert("thats corecct i'm fresh graduated so i'm don't have work experience!");
+
+  }
+  alert("thank you for visiting my page " + userName);
+
+  
+
+
