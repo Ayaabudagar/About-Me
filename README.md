1996dd06bdc2536c5a5e3d4fbb9096a2ee4ded2f
ahmadhomsi <homsiahmad111@gmail.com>
2021-04-14 14:52:34 +0300

    add new new function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # About-Me
 This webpage build to show some information about me  . i mintioned ten places i love it and i hope to visit it , i ask the user to guss number , enter it and alert the true number i want i ask the user to enter the color i love it and check if the color is true or false i added the score to the correct user answer
+
+navigator was Aya abudagar and the driver was ahmad al homsi 
+we refacotr the about me page togather and add new function 
--- js/app.js
+++ js/app.js
@@ -1,82 +1,111 @@
 'use strict';
-let score = 0 ;
+let score = 0;
 let userName = prompt("please enter your name", "your name").toLowerCase();
 // console.log(userName);
 alert("thank you for visiting my page " + userName);
 
-let myName = prompt ("my name is aya?");
-if (myName.toLowerCase()==="yes"||myName.toLowerCase()==="y") {
+let myName = prompt("my name is aya?");
+const theName = function (name) {
+  if (myName.toLowerCase() === "yes" || myName.toLowerCase() === "y") {
     // console.log("thats corecct my name is aya!");
     alert("thats corecct my name is aya!");
-    score++ ;
-}  else if (myName.toLowerCase()==="no"||myName.toLowerCase()==="n") {
-//    console.log("thats false my name is aya!");
+    score++;
+  } else if (myName.toLowerCase() === "no" || myName.toLowerCase() === "n") {
+    //    console.log("thats false my name is aya!");
     alert("thats false my name is aya!");
 
   }
+}
+theName(myName);
 
 
-  let myAge = prompt ("my age is 23?");
-if (myAge.toLowerCase()==="yes"||myAge.toLowerCase()==="y") {
+
+let myAge = prompt("my age is 23?");
+const theAge = function (age) {
+  if (myAge.toLowerCase() === "yes" || myAge.toLowerCase() === "y") {
     // console.log("thats corecct my age is 23!");
     alert("thats corecct my age is 23!");
     score++;
-}  else if (myAge.toLowerCase()==="no"||myAge.toLowerCase()==="n") {
+  } else if (myAge.toLowerCase() === "no" || myAge.toLowerCase() === "n") {
     // console.log ("thats false my age is 23!");
     alert("thats false my age is 23!");
 
   }
 
-  let myEducation = prompt (" did i study architecture in university?");
-if (myEducation.toLowerCase()==="yes"||myEducation.toLowerCase()==="y") {
+}
+theAge(myAge);
+
+
+
+let myEducation = prompt(" did i study architecture in university?");
+const theEducation = function (education) {
+  if (myEducation.toLowerCase() === "yes" || myEducation.toLowerCase() === "y") {
     // console.log("thats corecct i'm studied architecture in university!");
     alert("thats corecct i'm studied architecture in university!");
     score++;
-}  else if (myEducation.toLowerCase()==="no"||myEducation.toLowerCase()==="n") {
-//    console.log("thats false i'm  studied architecture in university !");
+  } else if (myEducation.toLowerCase() === "no" || myEducation.toLowerCase() === "n") {
+    //    console.log("thats false i'm  studied architecture in university !");
     alert("thats false i'm  studied architecture in university !");
 
+
   }
-  
-  let myGraduation = prompt (" did i graduate from  university?");
-if (myGraduation.toLowerCase()==="yes"||myGraduation.toLowerCase()==="y") {
+
+
+}
+theEducation(myEducation);
+
+let myGraduation = prompt(" did i graduate from  university?");
+const theGraduation = function (graduation) {
+  if (myGraduation.toLowerCase() === "yes" || myGraduation.toLowerCase() === "y") {
     // console.log("thats corecct i'm graduated from university!");
     alert("thats corecct i'm graduated from university!");
     score++;
-  }  else if (myGraduation.toLowerCase()==="no"||myGraduation.toLowerCase()==="n") {
+  } else if (myGraduation.toLowerCase() === "no" || myGraduation.toLowerCase() === "n") {
     // console.log("thats false i'm  graduated from university!");
     alert("thats false i'm  graduated from university!");
 
   }
-   
-  let myExperience = prompt (" did i have a work experience ?");
-if (myExperience.toLowerCase()==="yes"||myExperience.toLowerCase()==="y") {
-  // console.log("thats false i'm fresh graduated so i'm don't have work experience!");
+
+
+}
+theGraduation(myGraduation);
+
+
+let myExperience = prompt(" did i have a work experience ?");
+const theExperience = function (experience) {
+  if (myExperience.toLowerCase() === "yes" || myExperience.toLowerCase() === "y") {
+    // console.log("thats false i'm fresh graduated so i'm don't have work experience!");
     alert("thats false i'm fresh graduated so i'm don't have work experience!");
-}  else if (myExperience.toLowerCase()==="no"||myExperience.toLowerCase()==="n") {
-//    console.log("thats corecct i'm fresh graduated so i'm don't have work experience!");
+  } else if (myExperience.toLowerCase() === "no" || myExperience.toLowerCase() === "n") {
+    //    console.log("thats corecct i'm fresh graduated so i'm don't have work experience!");
     alert("thats corecct i'm fresh graduated so i'm don't have work experience!");
     score++;
   }
-  alert("thank you for visiting my page " + userName);
-  
 
-let guessNmber = 'num' ;
-function q6 () {
-  guessNmber = prompt (' guess ! what is the number in my mind ?') ;
+
+}
+theExperience(myExperience);
+
+
+alert("thank you for visiting my page " + userName);
+
+
+let guessNmber = 'num';
+function guessTheNmber() {
+  guessNmber = prompt(' guess ! what is the number in my mind ?');
   guessNmber = (guessNmber);
-  for (let i =1;i < 4;i++) {
-    if ((guessNmber)=== 15) {
-      alert ('great , the number is 15');
+  for (let i = 1; i < 4; i++) {
+    if ((guessNmber) === 15) {
+      alert('great , the number is 15');
       console.log('great , the number is 15');
       score++;
       break;
     } else if ((guessNmber) < 15) {
-      alert ('too low');
-      console.log ('too low');
-      guessNmber = prompt ('guess another number greater than this number');
-    } else if ((guessNmber) > 15 ) {
-      alert ('too high');
+      alert('too low');
+      console.log('too low');
+      guessNmber = prompt('guess another number greater than this number');
+    } else if ((guessNmber) > 15) {
+      alert('too high');
       console.log('too high');
       guessNmber = prompt('guess another number less than this number');
     }
@@ -84,58 +113,32 @@ function q6 () {
   }
   alert('15 is the correct answer');
   console.log('15 is the correct answer');
-} q6();
+} 
+guessTheNmber();
 
 
-let myColor= 'color';
-function q7(){
+let myColor = 'color';
+function guessColor() {
   myColor = ['blue', 'red'];
-  for (let c=0 ; c<6 ; c++){
+  for (let c = 0; c < 6; c++) {
     let userColor = prompt('my favarout color is ?  ');
     userColor = userColor.toLowerCase();
 
-    if(userColor === myColor[0] || userColor === myColor[1] ){
+    if (userColor === myColor[0] || userColor === myColor[1]) {
       alert('its right this is one of my colors');
       console.log('its right this is one of my colors');
       score++;
       break;
-    }else {
+    } else {
       alert('its wrong this is not one of my colors');
       console.log('its wrong this is not one of my colors');
     }
   }
-  alert('me best colors is ' + myColor);
+  alert('my best colors is ' + myColor);
   console.log('me best colors is ' + myColor);
-  alert('your score is ' + score ) ;
-  console.log('your score is ' + score ) ;
+  alert('your score is ' + score);
+  console.log('your score is ' + score);
   alert('welcom ' + userName);
   console.log('welcom ' + userName);
-} q7();
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
+}
+ guessColor();
